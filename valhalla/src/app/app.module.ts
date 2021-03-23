f137169f931f3bffc68c4812eecc769b445e626e
wpuerner <williampuerner@gmail.com>
2021-03-23 08:36:38 -0500

    carrot spinner for loading server status

--- valhalla/src/app/app.module.ts
+++ valhalla/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ServerStatusComponent } from './server-status/server-status.component';
 import { HttpClientModule } from '@angular/common/http';
 import { StateButtonComponent } from './state-button/state-button.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerStatusComponent,
-    StateButtonComponent
+    StateButtonComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
