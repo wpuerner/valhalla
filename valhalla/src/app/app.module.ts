4030d4c9334d15c55a675755fd1697f8fb967423
wpuerner <williampuerner@gmail.com>
2021-03-23 08:56:09 -0500

    remove spinner component for more basic impl

--- valhalla/src/app/app.module.ts
+++ valhalla/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent } from './app.component';
 import { ServerStatusComponent } from './server-status/server-status.component';
 import { HttpClientModule } from '@angular/common/http';
 import { StateButtonComponent } from './state-button/state-button.component';
-import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerStatusComponent,
-    StateButtonComponent,
-    SpinnerComponent
+    StateButtonComponent
   ],
   imports: [
     BrowserModule,
